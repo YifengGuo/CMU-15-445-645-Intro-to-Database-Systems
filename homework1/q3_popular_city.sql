5f33d80a2a30c8fd4ad9ae078139a8c031ecd2aa
YifengGuo <gyf940517@gmail.com>
2018-11-28 00:25:04 +0800

    q3 partial solution

--- homework1/q3_popular_city.sql
+++ homework1/q3_popular_city.sql
@@ -1,6 +1,2 @@
-SELECT city, ROUND((scount * 1.0 / (SELECT COUNT(*) FROM trip)), 4) AS ratio FROM 
-(SELECT city, COUNT(station_id) AS scount FROM station 
-INNER JOIN trip 
-WHERE station.station_id = trip.start_station_id OR station.station_id = trip.end_station_id
-GROUP BY city)
-ORDER BY ratio DESC, city ASC;
+select count(*) from (select * from (select id, city as start_city from trip left join station where station.station_id = trip.start_station_id) as A left join 
+(select id, city as end_city from trip left join station where station.station_id = trip.end_station_id) as B where A.id = B.id) where start_city != end_city;
